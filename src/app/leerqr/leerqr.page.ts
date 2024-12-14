f228ffe994cbad4f8fd9c17798499b092ad619b5
Flamingopanic <163334358+Flamingopanic@users.noreply.github.com>
2024-12-14 20:59:56 -0300

    Lector qr
    Lector qr finalizado

--- src/app/leerqr/leerqr.page.ts
+++ src/app/leerqr/leerqr.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { LoadingController, ModalController, Platform } from '@ionic/angular';
+import { AlertController, LoadingController, ModalController, Platform, ToastController } from '@ionic/angular';
 import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
 import { LensFacing, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
+import { Clipboard } from '@capacitor/clipboard';
+import { Browser } from '@capacitor/browser';
 
 @Component({
   selector: 'app-leerqr',
@@ -15,7 +17,9 @@ export class LeerqrPage implements OnInit {
   constructor(
     private modalController: ModalController,
     private platform: Platform,
-    private loadingController: LoadingController
+    private loadingController: LoadingController,
+    private toastController: ToastController,
+    private alertController: AlertController
 
   ) { }
 
@@ -46,5 +50,53 @@ export class LeerqrPage implements OnInit {
       BarcodeScanner.removeAllListeners().then();
     }
   }
+  
+  writeToClipboard = async () => {
+    await Clipboard.write({
+      string: this.scanResult
+    });
 
+    const toast = await this.toastController.create({
+      message: 'Copiado',
+      duration: 2000,
+      color: 'tertiary',
+      icon:  'clipboard-outline',
+      position: 'middle'
+    });
+    toast.present();
+  };
+
+  isUrl(){
+    let regex = /\.(com|net|io|me|crypto|ai)\b/i;
+    return regex.test(this.scanResult);
+  }
+  
+
+openCapacitorSite = async () => {
+
+    const alert = await this.alertController.create({
+      header: 'Confirm!',
+      message: 'Quieres abrir este enlace en el navegador?',
+      buttons: [
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }, {
+          text: 'Okay',
+          handler: async() => {
+            let url = this.scanResult;
+
+            if(!['https://'].includes(this.scanResult)) url ='https://'+ this.scanResult
+
+            await Browser.open({ url });
+          }
+        }
+      ]
+    });
+  
+    await alert.present();
+  
+
+  
+};
 }
