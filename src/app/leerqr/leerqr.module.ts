9bbbb6fc86ec490745f28ae7dcd4e776c4ee4c49
Flamingopanic <163334358+Flamingopanic@users.noreply.github.com>
2024-12-14 19:32:01 -0300

    Qr
    Se agrega funcion de leer y generar qr

--- src/app/leerqr/leerqr.module.ts
+++ src/app/leerqr/leerqr.module.ts
@@ -7,6 +7,7 @@ import { IonicModule } from '@ionic/angular';
 import { LeerqrPageRoutingModule } from './leerqr-routing.module';
 
 import { LeerqrPage } from './leerqr.page';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
 
 @NgModule({
   imports: [
@@ -15,6 +16,6 @@ import { LeerqrPage } from './leerqr.page';
     IonicModule,
     LeerqrPageRoutingModule
   ],
-  declarations: [LeerqrPage]
+  declarations: [LeerqrPage,BarcodeScanningModalComponent]
 })
 export class LeerqrPageModule {}
--- src/app/leerqr/leerqr.page.ts
+++ src/app/leerqr/leerqr.page.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { LoadingController, ModalController, Platform } from '@ionic/angular';
+import { BarcodeScanningModalComponent } from './barcode-scanning-modal.component';
+import { LensFacing, BarcodeScanner } from '@capacitor-mlkit/barcode-scanning';
 
 @Component({
   selector: 'app-leerqr',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LeerqrPage implements OnInit {
 
-  constructor() { }
+  scanResult = '';
+  
+  constructor(
+    private modalController: ModalController,
+    private platform: Platform,
+    private loadingController: LoadingController
 
-  ngOnInit() {
+  ) { }
+
+  async startScan(){
+    const modal = await this.modalController.create({
+      component: BarcodeScanningModalComponent,
+      cssClass: 'barcode-scanning-modal',
+      showBackdrop: false,
+      componentProps: { 
+        formats: [],
+        lensFacing: LensFacing.Back
+      }
+    });
+    
+    await modal.present();
+
+    const { data } = await modal.onWillDismiss();
+
+    if(data){
+      this.scanResult = data?.barcode?.displayValue;
+    }
+  }
+
+  ngOnInit(): void {
+    if(this.platform.is('capacitor')){
+      BarcodeScanner.isSupported().then();
+      BarcodeScanner.checkPermissions().then();
+      BarcodeScanner.removeAllListeners().then();
+    }
   }
 
 }
